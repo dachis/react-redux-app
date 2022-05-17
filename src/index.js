49b3064eab2443a28323f91fc0b00098f46f7121
sasaki <sasaki.tatsuya@headwaters.co.jp>
2022-05-17 11:35:38 +0900

    fix render function

--- src/index.js
+++ src/index.js
@@ -10,19 +10,19 @@ import reportWebVitals from './reportWebVitals';
 
 const store = createStore(reducer)
 
-ReactDOM.render(
+// ReactDOM.render(
+//   <Provider store={store}>
+//     <App />
+//   </Provider>,
+//   document.getElementById('root')
+// );
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
   <Provider store={store}>
     <App />
-  </Provider>,
-  document.getElementById('root')
+  </Provider>
 );
-
-// const root = ReactDOM.createRoot(document.getElementById('root'));
-// root.render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>
-// );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
